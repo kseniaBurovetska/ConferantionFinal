1c0fc27d04a0e6b885497c9b5932a797ad90e5b2
kburovetska <kburovetska@ukr.net>
2019-04-01 21:40:25 +0300

    Created connection properties

--- src/main/java/model/dao/impl/ConnectionPoolHolder.java
+++ src/main/java/model/dao/impl/ConnectionPoolHolder.java
@@ -3,6 +3,10 @@ package model.dao.impl;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 import javax.sql.DataSource;
+import java.io.*;
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.Properties;
 
 public class ConnectionPoolHolder {
 
@@ -12,19 +16,27 @@ public class ConnectionPoolHolder {
         if (dataSource == null) {
             synchronized (ConnectionPoolHolder.class) {
                 if (dataSource == null) {
+
                     try {
-                        Class.forName("com.mysql.jdbc.Driver");
-                    } catch (ClassNotFoundException e) {
+                        Properties properties = new Properties();
+
+                        properties.load(new FileInputStream(
+                                "D:\\Projects\\Idea\\Training\\ConferenceFinal\\src\\main\\resources\\db.properties"));
+
+
+                        Class.forName(properties.getProperty("db.connection.driver"));
+                        BasicDataSource ds = new BasicDataSource();
+                        ds.setUrl(properties.getProperty("db.connection.url"));
+                        ds.setUsername(properties.getProperty("db.connection.username"));
+                        ds.setPassword(properties.getProperty("db.connection.password"));
+                        ds.setMinIdle(5);
+                        ds.setMaxIdle(10);
+                        ds.setMaxOpenPreparedStatements(100);
+                        dataSource = ds;
+
+                    } catch (IOException | ClassNotFoundException e) {
                         e.printStackTrace();
                     }
-                    BasicDataSource ds = new BasicDataSource();
-                    ds.setUrl("jdbc:mysql://localhost:3306/conferentiondb");
-                    ds.setUsername("root");
-                    ds.setPassword("root");
-                    ds.setMinIdle(5);
-                    ds.setMaxIdle(10);
-                    ds.setMaxOpenPreparedStatements(100);
-                    dataSource = ds;
                 }
             }
         }
